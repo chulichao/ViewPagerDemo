98b808100b7930ca1b259e200f0235d825ff4d68
chulc1114 <chulc1114@thundersoft.com>
2020-11-25 18:18:22 +0800

    addOnPageChangeListener

--- app/src/main/java/com/clc/vpd/MainActivity.java
+++ app/src/main/java/com/clc/vpd/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ViewPager mViewPager;
+    private Button mHomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +21,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mViewPager = findViewById(R.id.vp_container);
+        mHomeButton = findViewById(R.id.btn_back_home);
+
 
         List<String> list = new ArrayList();
         list.add("1");
         list.add("2");
         list.add("3");
+        list.add("4");
+        list.add("5");
+        list.add("6");
+        list.add("7");
+        list.add("8");
+        list.add("9");
         MyPagerAdapter pagerAdapter = new MyPagerAdapter(this, list);
         mViewPager.setAdapter(pagerAdapter);
+
+        mHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(0);
+            }
+        });
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            private static final String TAG = "OnPageChangeListener";
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.d(TAG, "onPageScrolled: position = " + position + ", positionOffset = " + positionOffset + ", positionOffsetPixels = " + positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d(TAG, "onPageSelected: position = " + position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                Log.d(TAG, "onPageScrollStateChanged: state = " + state);
+            }
+        });
     }
 }
